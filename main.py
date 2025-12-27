8a47e53192e39cb49f62de08f4ce1036d64313a4
VahidKianiCoding <165814462+VahidKianiCoding@users.noreply.github.com>
2025-12-27 11:59:41 +0330

    Add validation checks for character stats

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def create_character(name, strength, intelligence, charisma):
         print("The character should have a name")
     if len(name) > 10:
         print("The character name is too long")
+
+    if not any(isinstance(s, int) for s in stats):
+        print("All stats should be integers")
+    if any(for s in stats < 1):
+        print("All stats should be no less than 1")
+    if any(for s in stats > 4):
+        print("All stats should be no more than 4")
+    if sum(stats) != 7:
+        print("The character should start with 7 points")
